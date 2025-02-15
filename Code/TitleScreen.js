803317968b46cad64b71bb2085fa02ac33dcda1d
Diogodias28 <116687720+Diogodias28@users.noreply.github.com>
2025-02-15 16:32:57 +0000

    Add files via upload

---
+++ Code/TitleScreen.js
@@ -0,0 +1,16 @@
+class TitleScreen extends Phaser.Scene {
+  constructor() {
+    super({ key: 'bootgame' });
+  }
+
+  preload() {
+    this.load.image('background', 'assets/background1.jpg');
+  }
+
+  create() {
+    this.add.text(10, 10, 'Loading game...', { fill: '#0f0' });
+    this.input.on('pointerdown', () => {
+      this.scene.start('playgame');
+    });
+  }
+}
